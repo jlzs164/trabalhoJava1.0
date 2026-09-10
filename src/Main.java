78ba16715e2c42ea1fe40b4df7c370b9279ac6ec
João Luiz Silveira Maciel <joaolmaciel1604@gmail.com>
2026-09-10 09:35:54 -0300

    corrigido pra versao java 21

--- src/Main.java
+++ src/Main.java
@@ -1,44 +1,48 @@
-void main() {
+import java.util.Scanner;
 
-    Scanner entrada = new Scanner(System.in);
+public class Main {
+    public static void main(String[] args) {
+        Scanner entrada = new
+                Scanner(System.in);
 
-    IO.println("Digite uma mensagem:");
-    String mensagem = entrada.nextLine().toLowerCase();
+        System.out.println("Digite uma mensagem:");
+        String mensagem = entrada.nextLine().toLowerCase();
 
-    int pontos = 0;
+        int pontos = 0;
 
-    String[] palavrasSuspeitas = {
-            "acesse o link",
-            "clique aqui",
-            "conta será bloqueada",
-            "confirme seus dados",
-            "você ganhou",
-            "receba seu prêmio",
-            "confirme sua senha"
-    };
+        String[] palavrasSuspeitas = {
+                "acesse o link",
+                "clique aqui",
+                "conta será bloqueada",
+                "confirme seus dados",
+                "você ganhou",
+                "receba seu prêmio",
+                "confirme sua senha"
+        };
 
-    int[] pontosPalavras = {
-            1,
-            1,
-            2,
-            2,
-            3,
-            4,
-            5
-    };
+        int[] pontosPalavras = {
+                1,
+                1,
+                2,
+                2,
+                3,
+                4,
+                5
+        };
 
-    for (int i = 0; i < palavrasSuspeitas.length; i++) {
-        if (mensagem.contains(palavrasSuspeitas[i])) {
-            pontos += pontosPalavras[i];
-            IO.println("Pontos de suspeita: " + pontos);
+        for (int i = 0; i < palavrasSuspeitas.length; i++) {
+            if (mensagem.contains(palavrasSuspeitas[i])) {
+                pontos += pontosPalavras[i];
+                System.out.println("Pontos de suspeita: " + pontos);
+            }
         }
-    }
 
-    if (pontos >= 4) {
-        IO.println("Sinais suspeitos encontrados.");
-    } else if (pontos >= 2) {
-        IO.println("Baixa suspeita.");
-    } else {
-        IO.println("Mínima suspeita.");
+        if (pontos >= 4) {
+            System.out.println("Sinais suspeitos encontrados.");
+        } else if (pontos >= 2) {
+            System.out.println("Baixa suspeita.");
+        } else {
+            System.out.println("Mínima suspeita.");
+        }
     }
 }
